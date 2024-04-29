60f9ceba76b5d7a863cecc5fe8befede6f00f3f3
sandeepmore-code <152766192+sandeepmore-code@users.noreply.github.com>
2024-04-29 14:44:52 +0530

    updates

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+ <Routes>
+  <Route path='/' element= {<Home />}/>
+  <Route path='/login' element = {<Login />}/>
+ </Routes>
+    </div>
+  );
+}
+
+export default App;
